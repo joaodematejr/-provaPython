509b34ba61de8279b497e981295b76701af77619
João Dematé Jr <joaodematejr@gmail.com>
2020-09-23 20:53:24 -0300

    Update 02.py

--- python/02.py
+++ python/02.py
@@ -9,25 +9,25 @@ class Funcionario:
         self.salario = self.salario + self.aumento
 
 
-class programador(Funcionario):
+class Programador(Funcionario):
     def __init__(self, nome, idade, salario):
         Funcionario.__init__(self, nome, idade, salario)
         self.aumento = 20
 
 
-class analista(Funcionario):
+class Analista(Funcionario):
     def __init__(self, nome, idade, salario):
         Funcionario.__init__(self, nome, idade, salario)
         self.aumento = 30
 
 
-p1 = programador("João", 20, 1000)
+p1 = Programador("João", 20, 1000)
 print(p1.nome)
 print(p1.salario)
 p1.aumenta_salario()
 print(p1.salario)
 
-a1 = analista("Gisele", 18, 1500)
+a1 = Analista("Gisele", 18, 1500)
 print(a1.nome)
 print(a1.salario)
 a1.aumenta_salario()
